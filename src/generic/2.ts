50657b3c99277227cd864be1792b0dab64a0f539
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-01-06 14:41:46 +0200

    fix

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -14,7 +14,7 @@ type AllType = {
 function compare(
   top: Pick<AllType, "name" | "color">,
   bottom: Pick<AllType, "position" | "weight">
-) {
+): AllType {
   return {
     name: top.name,
     color: top.color,
